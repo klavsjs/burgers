d2a2737f9d35dece3d535ddaf9be4671be012a46
Klavs Jurciks <klavs.jurciks@gmail.com>
2024-11-07 13:49:42 +0200

    Fix TS errors caused by misconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
 import tailwindcss from "tailwindcss"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
